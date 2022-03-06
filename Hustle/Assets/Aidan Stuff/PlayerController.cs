e7aa45575d83ce0cefeca3b9be1e25e37ad6e147
apmolina <apmolina@iu.edu>
2022-03-06 12:08:08 -0500

    player controller scripts added

---
+++ Hustle/Assets/Aidan Stuff/PlayerController.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Player
+{
+    Vector2 move;
+    public float jumpTakeOffSpeed = 3f;
+    int speedState = 0;
+    bool ticked;
+    float tickTimer = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = Vector2.zero;
+        ticked = false;
+    }
+
+    protected override void ComputeVelocity(){
+
+        if(Input.GetKeyDown("right")){
+            speedState += 1;
+            if(speedState > 3){
+                speedState = 3;
+            }
+        }
+        if(Input.GetKeyDown("left")){
+            speedState -= 1;
+            if(speedState < -3){
+                speedState = -3;
+            }
+        }
+
+        maxSpeed(speedState);
+
+        if(Input.GetKeyDown("up") && grounded){
+            velocity.y = jumpTakeOffSpeed + Mathf.Abs(move.x/2.5f);
+        }
+        else if(Input.GetKeyUp("up")){
+            if(velocity.y > 0f){
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        targetVelocity = move;
+    }
+
+    void maxSpeed(int state){
+        if(ticked == true){
+            tickTimer -= Time.deltaTime;
+            if(tickTimer <= 0.0f){
+                ticked = false;
+                tickTimer = 0.1f;
+            }
+        }
+
+        else if(state == -3 && ticked == false){
+            if(move.x > -15f){
+                move.x -= 0.5f;
+                ticked = true;
+            }
+            
+            if(move.x < -15f){
+                move.x = -15f;
+            }
+        }
+        
+        else if(state == -2 && ticked == false){
+            if(move.x > -10f){
+                move.x -= 0.5f;
+                ticked = true;
+            }
+            
+            if(move.x < -10f){
+                move.x += 0.5f;
+                ticked = true;
+            }
+        }
+
+        else if(state == -1 && ticked == false){
+            if(move.x > -5f){
+                move.x -= 0.5f;
+                ticked = true;
+            }
+            
+            if(move.x < -5f){
+                move.x += 0.5f;
+                ticked = true;
+            }
+        }
+
+        else if(state == 0 && ticked == false){
+            if(move.x > 0f){
+                move.x -= 0.5f;
+                ticked = true;
+            }
+            
+            if(move.x < 0f){
+                move.x += 0.5f;
+                ticked = true;
+            }
+        }
+
+        else if(state == 1 && ticked == false){
+            if(move.x > 5f){
+                move.x -= 0.5f;
+                ticked = true;
+            }
+            
+            if(move.x < 5f){
+                move.x += 0.5f;
+                ticked = true;
+            }
+        }
+
+        else if(state == 2 && ticked == false){
+            if(move.x > 10f){
+                move.x -= 0.5f;
+                ticked = true;
+            }
+            
+            if(move.x < 10f){
+                move.x += 0.5f;
+                ticked = true;
+            }
+        }
+
+        else if(state == 3 && ticked == false){
+            if(move.x > 15f){
+                move.x = 15f;
+            }
+            
+            if(move.x < 15f){
+                move.x += 0.5f;
+                ticked = true;
+            }
+        }
+    }
+}
