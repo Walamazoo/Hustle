dea1f8fc43d4e69010d35dc541cb7dd5c36bcd61
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2022-03-29 18:34:02 -0400

    Updated/fixed respawn system

--- Hustle/Assets/Steven Stuff/Scripts/Checkpoint.cs
+++ Hustle/Assets/Steven Stuff/Scripts/Checkpoint.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    [SerializeField] int respawnSpeed;
+    public int respawnSpeed;
     [SerializeField] Respawn respawn;
+    [SerializeField] int respawnIndex;
+    private int _currentIndex = 0;
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
             Debug.Log("checkpoint");
-            respawn.currentCheckpoint = gameObject;
+            if(this.respawnIndex > _currentIndex){
+                _currentIndex = this.respawnIndex;
+                respawn.currentCheckpoint = this;
+            }
         }
     }
 }
--- Hustle/Assets/Steven Stuff/Scripts/Respawn.cs
+++ Hustle/Assets/Steven Stuff/Scripts/Respawn.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    [SerializeField] GameObject playerPrefab;
+    [SerializeField] PlayerController player;
 
-    public GameObject currentCheckpoint;
+    public Checkpoint currentCheckpoint;
+    private int _speedState = 0;
+
+     void Start()
+    {
+        GameEvents.current.OnSpeedStateChange += UpdateSpeedState;
+    }
+
+    void UpdateSpeedState(int direction){
+        Debug.Log("Update speed state");
+        if(_speedState !>= 3 || _speedState !<= -3){
+            _speedState += direction;
+        }
+        Debug.Log(_speedState);
+    }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Player")){
             Debug.Log("respawn");
-            Destroy(other.gameObject.transform.parent);
-            Instantiate(playerPrefab, new Vector2(currentCheckpoint.transform.position.x, currentCheckpoint.transform.position.y+1), Quaternion.identity);
-            //put player's speed to the checkpoint's respawnSpeed value
+            other.gameObject.transform.parent.gameObject.transform.position = new Vector2(currentCheckpoint.transform.position.x, currentCheckpoint.transform.position.y+1);
+            if (_speedState > currentCheckpoint.respawnSpeed){
+                Debug.Log("Decreasing speed state");
+                while(_speedState > currentCheckpoint.respawnSpeed){
+                    GameEvents.current.SpeedStateChange(-1);
+                    Debug.Log(_speedState);
+                }
+            }
+            else if(_speedState < currentCheckpoint.respawnSpeed){
+                Debug.Log("Increasing speed state");
+                while (_speedState < currentCheckpoint.respawnSpeed){
+                    GameEvents.current.SpeedStateChange(1);
+                    Debug.Log(_speedState);
+                }
+            }
         }
     }
 
