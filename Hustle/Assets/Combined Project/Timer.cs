a0e6e529c4ec536b6f889041f817ec843f8a3527
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2022-04-05 02:43:46 -0400

    TMP, UI update, Timer, Checkpoints in Combined Scene

---
+++ Hustle/Assets/Combined Project/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] bool timerIsRunning = false; 
+    [SerializeField] TextMeshProUGUI timerText;
+
+    private float time;
+    private float minutes;
+    private float seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+         if(timerIsRunning){
+            time += Time.deltaTime;
+            minutes = Mathf.FloorToInt(time / 60); 
+            seconds = Mathf.FloorToInt(time % 60);
+            timerText.text = string.Format("{0:00} {1:00}", minutes, seconds);
+        }
+    }
+}
